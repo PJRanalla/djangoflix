0acd6b51ec9b7ddc7ff9bcb6906a61e3b5e99a15
Patrick <patrickranalla@gmail.com>
2024-11-15 12:12:20 -0500

    updating env variables

--- netflix_clone/settings.py
+++ netflix_clone/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
+
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=Csv())
 
 INSTALLED_APPS = [
